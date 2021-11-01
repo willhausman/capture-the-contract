59be3fb5efb49cde45517128f39618355a77096a
Will Hausman <will.jhausman@outlook.com>
2021-11-01 12:37:22 -0500

    missed a usage

--- src/client/pages/Home.tsx
+++ src/client/pages/Home.tsx
@@ -1,13 +1,11 @@
 import { useState } from 'react';
-import { useCustomUtilHook } from '~/hooks';
 
 export const Home = () => {
   const [title] = useState('Template app');
-  const services = useCustomUtilHook();
 
   return (
     <div>
-      Welcome to your {title} {services.key}.
+      Welcome to your {title}.
     </div>
   );
 };
